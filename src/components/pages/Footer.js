3fb5a7179774849688ac4836dfc80845f60aba68
kristensantee <mikeandkris27@gmail.com>
2022-08-26 15:01:30 -0700

    deploy test

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ScienceIcon from '@mui/icons-material/Science';
 import PsychologyIcon from '@mui/icons-material/Psychology';
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import '../styles/Footer.css'
 
 export default function Footer() {
     return (
         <footer className='footer'>
+            <a>
+            
+            </a>
             <p>{<ScienceIcon/>} Made with React by Kristen {<PsychologyIcon/>}</p>
+            <a>
+
+            </a>
         </footer>
     )
 }
