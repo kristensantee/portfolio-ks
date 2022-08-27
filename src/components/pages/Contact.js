b2293b4b68bd987bfaadb8bc16860ec54c8e9d00
kristensantee <mikeandkris27@gmail.com>
2022-08-26 20:22:01 -0700

    added projects component

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,10 @@
 import React, {useState} from 'react'
 import Box from '@mui/material/Box'
-import { makeStyles } from '@mui/material/styles'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
 import TextField from '@mui/joy/TextField'
-import Divider from '@mui/material/Divider'
 import Button from '@mui/joy/Button'
-import GitHubIcon from '@mui/icons-material/GitHub'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
-import { validateEmail } from '../../utils/helpers'
+import { validateEmail,checkName } from '../../utils/helpers'
+import '../styles/Contact.css'
 
 export default function Contact() {
 
@@ -37,9 +29,12 @@ export default function Contact() {
 
     const handleFormSubmit = (e) => {
         e.preventDefauit();
-        if (!validateEmail(email) || !name) {
-            setErrorMessage('Email or name not valid. Please provide a valid email address and name.');
+        if (!validateEmail(email)) {
+            setErrorMessage('Email not valid. Please provide a valid email address.');
         return;
+        }
+        if (!checkName(name)) {
+            setErrorMessage('Please provide a name.')
         }
         alert(`Thanks for the message, ${name}`)
         setEmail('');
@@ -48,74 +43,55 @@ export default function Contact() {
     };
 
     return (
-    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <div>
-            <h1>Let's connect!</h1>
-            <nav aria-label="main mailbox folders">
-                <List>
-                <ListItem disablePadding>
-                    <ListItemButton component='a' href="https://github.com/kristensantee" target="_blank">
-                    <ListItemIcon>
-                        <GitHubIcon />
-                    </ListItemIcon>
-                    <ListItemText primary='Github' />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton component='a' href="https://www.linkedin.com/in/kristen-santee-aba1a631/" target="_blank">
-                    <ListItemIcon>
-                        <LinkedInIcon />
-                    </ListItemIcon>
-                    <ListItemText primary='LinkedIn' />
-                    </ListItemButton>
-                </ListItem>
-                <Divider />
-                    <h3>Contact Kristen</h3>
-                        <form className='form'>
-                            <TextField
-                                value={name}
-                                label='name'
-                                name='name'
-                                onChange={handleInputChange}
-                                type='name'
-                                placeholder='name'
-                                size='md'
-                                variant='outlined'
-                                color='primary'
-                                required
-                                />
-                            <TextField
-                                value={email}
-                                label='email'
-                                name='email'
-                                onChange={handleInputChange}
-                                type='email'
-                                placeholder='email'
-                                size='md'
-                                variant='outlined'
-                                required
-                                />
-                            <TextField
-                                value={message}
-                                label='message'
-                                name='message'
-                                onChange={handleInputChange}
-                                type='text'
-                                placeholder='write your message here'
-                                size='md'
-                                variant='outlined'
-                                required
-                                />
-                            <Button type='button' variant='outlined' endIcon={<KeyboardArrowRight/>} color='primary' onClick={handleFormSubmit}>Submit</Button>
-                        </form>
-                        {errorMessage && (
-                            <div>
-                                <p className='error-text'>{errorMessage}</p>
-                            </div>
-                        )}  
-                </List>
-            </nav>
-      </div>
+    <Box sx={{ width: '80%'}} className='formContainer'>
+        <h1>Let's connect!</h1>
+        <form >
+        <h3>Contact Kristen</h3>
+            <TextField
+                value={name}
+                label='name'
+                name='name'
+                onChange={handleInputChange}
+                type='name'
+                placeholder='name'
+                size='md'
+                variant='outlined'
+                color='primary'
+                required
+                className='textField'
+                />
+            <TextField
+                value={email}
+                label='email'
+                name='email'
+                onChange={handleInputChange}
+                type='email'
+                placeholder='email'
+                size='md'
+                variant='outlined'
+                required
+                className='textField'
+                />
+            <TextField
+                value={message}
+                label='message'
+                name='message'
+                onChange={handleInputChange}
+                type='text'
+                placeholder='write your message here'
+                size='md'
+                variant='outlined'
+                required
+                className='textField'
+                id='message'
+            />
+            <Button type='button' variant='outlined' className="button" endIcon={<KeyboardArrowRight/>} color='primary' onClick={handleFormSubmit} >Submit</Button>
+        </form>
+        {errorMessage && (
+            <div>
+                <p className='error-text'>{errorMessage}</p>
+            </div>
+        )}
     </Box>
     )
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import ButtonBase from '@mui/material/ButtonBase';
-import Typography from '@mui/material/Typography';
+import Projects from './Projects'
 import curarte from '../../images/curARTe.png'
 import outbound from '../../images/outbound.png'
 import staticportfolio from '../../images/staticportfolio.png'
@@ -10,134 +7,51 @@ import weatherapp from '../../images/weatherapp.png'
 import regex from '../../images/regex.png'
 import pwa from '../../images/pwa.png'
 
-const images = [
+const projects = [
     {
-      url: outbound,
-      title: 'Outbound Gear Exchange',
-      width: '40%',
-    },
-    {
-      url: curarte,
-      title: 'curARTe',
-      width: '30%',
+    name: 'Outbound Gear Exchange',
+    description: 'oge description here',
+    id: 1,
+    image: outbound
     },
     {
-      url: staticportfolio,
-      title: 'Static Portfolio',
-      width: '30%',
+    name: 'curARTe',
+    description: 'curARTe description here',
+    id: 2,
+    image: curarte
     },
     {
-        url: weatherapp,
-        title: 'Weather App',
-        width: '40%',
+    name: 'Static Portfolio',
+    description: 'static portfolio description here',
+    id: 3,
+    image: staticportfolio
     },
     {
-        url: regex,
-        title: 'regex writeup',
-        width: '30%',
+    name: 'Weather App',
+    description: 'weather app description here',
+    id: 4,
+    image: weatherapp
     },
     {
-        url: pwa,
-        title: 'Progressive Web App',
-        width: '30%',
+    name: 'Regex Writeup',
+    description: 'regex description here',
+    id: 5,
+    image: regex
     },
-  ];
-  
-  const ImageButton = styled(ButtonBase)(({ theme }) => ({
-    position: 'relative',
-    height: 200,
-    [theme.breakpoints.down('sm')]: {
-      width: '100% !important', // Overrides inline-style
-      height: 100,
+    {
+    name: 'PWA',
+    description: 'pwa description here',
+    id: 6,
+    image: pwa
     },
-    '&:hover, &.Mui-focusVisible': {
-      zIndex: 1,
-      '& .MuiImageBackdrop-root': {
-        opacity: 0.15,
-      },
-      '& .MuiImageMarked-root': {
-        opacity: 0,
-      },
-      '& .MuiTypography-root': {
-        border: '4px solid currentColor',
-      },
-    },
-  }));
-  
-  const ImageSrc = styled('span')({
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center 40%',
-  });
-  
-  const Image = styled('span')(({ theme }) => ({
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.palette.common.white,
-  }));
-  
-  const ImageBackdrop = styled('span')(({ theme }) => ({
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundColor: theme.palette.common.black,
-    opacity: 0.4,
-    transition: theme.transitions.create('opacity'),
-  }));
-  
-  const ImageMarked = styled('span')(({ theme }) => ({
-    height: 3,
-    width: 18,
-    backgroundColor: theme.palette.common.white,
-    position: 'absolute',
-    bottom: -2,
-    left: 'calc(50% - 9px)',
-    transition: theme.transitions.create('opacity'),
-  }));
-  
-  export default function ButtonBases() {
+]
+
+export default function Portfolio() {
     return (
-      <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
-        {images.map((image) => (
-          <ImageButton
-            focusRipple
-            key={image.title}
-            style={{
-              width: image.width,
-            }}
-          >
-            <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: 'relative',
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-              >
-                {image.title}
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        ))}
-      </Box>
-    );
-  }
+        <div>
+            <Projects name={projects.name[0]} description={projects.description[0]} id={projects.id[0]} image={projects.image[0]}/>
+            <Projects name={projects.name[1]} description={projects.description[1]} id={projects.id[1]}image={projects.image[1]}/>
+
+        </div>
+    )
+}
